6e545eb7b47c01e58c8a3b386359741c1ffc5403
Damien Hodgkin <dracul01@gmail.com>
2011-12-10 13:28:06 -0500

    some adjustments to the layout of the default app

--- app/partials/main.html.php
+++ app/partials/main.html.php
@@ -1,11 +1,11 @@
-<div>
+<div id="ayb">
     <p>Sombody set us up the bomb!!</p>
     <p>HA HA HA! All your base are belong to us!</p>
 </div>
 
-<div><a href="https://github.com/FourElementsDevelopment/pinp">Fork it!</a></div>
+<div id="pinpfork"><a href="https://github.com/FourElementsDevelopment/pinp">Fork it!</a> and help us get "Pinp of the Year"</div>
 
-<div>
+<div id="gitlive">
     <script src='http://gitlive.com/githublive.min.js'></script>
     <script>var GithubPush = {num_old:5,nodes:['FourElementsDevelopment/pinp']}</script>
     <div id='commits'></div>

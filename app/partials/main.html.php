3c4b1eeee0ae2d4992b23201850cefb6498d7d49
Damien Hodgkin <dracul01@gmail.com>
2011-12-10 12:58:33 -0500

    added github repo link

--- app/partials/main.html.php
+++ app/partials/main.html.php
@@ -2,6 +2,9 @@
     <p>Sombody set us up the bomb!!</p>
     <p>HA HA HA! All your base are belong to us!</p>
 </div>
+
+<div><a href="https://github.com/FourElementsDevelopment/pinp">Fork it!</a></div>
+
 <div>
     <script src='http://gitlive.com/githublive.min.js'></script>
     <script>var GithubPush = {num_old:5,nodes:['FourElementsDevelopment/pinp']}</script>
